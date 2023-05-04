1af7d636ab4de494641061ccdd8f64ad2c35a31e
LetsGitFunky <NickHeinBass@gmail.com>
2023-05-04 09:18:22 -0700

    ArtShow

---
+++ ArtShow/db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+Mike = User.new(name: 'Mike', email: 'mike@aa.com')
+Simba = User.new(name: "Simba", email: "simba@aa.com")
+Nala = User.new(name: "Nala", email: "nala@aa.com")
